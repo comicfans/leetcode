5f43c48b30f92dc30397ceac46ab38531ff52ecb
comicfans <comcifans44@gmail.com>
2023-11-22 14:09:50 +0200

    2462 still slow

--- cpp/2462_hire.cpp
+++ cpp/2462_hire.cpp
@@ -18,81 +18,53 @@ public:
             return accumulate(costs.begin(),costs.end(),(long long)0);
         }
 
-        map<int,set<int>> costPos;
 
-        for(int i=0;i<costs.size();++i){
-            costPos[costs[i]].insert(i);
+        vector<pair<int,int>> numberIdx;
+        numberIdx.resize(costs.size());
+        for(int i = 0;i<costs.size();++i){
+            numberIdx[i].first = costs[i];
+            numberIdx[i].second = i;
         }
 
+        sort(numberIdx.begin(), numberIdx.end(), [](auto lhs, auto rhs){
+            return lhs > rhs;
+        });
 
-        long long total = 0;
-
+        //search from back
+        //
         int leftPick = 0;
         int rightPick = 0;
-        for(int i =0;i<k;++i){
+        long long total = 0;
 
-            if(candidates >= costs.size() - leftPick - rightPick){
-
-                //picking left k - i value from head
-
-                int picked = 0;
-                for(auto &kv: costPos){
-
-                    int thisNumber = min((int)kv.second.size(), k-i - picked);
-                    picked += thisNumber;
-                    total += thisNumber * kv.first;
-                    
-                    if(picked == k - i){
-                        break;
-                    }
+        for(int i = 0; i< k;++i){
+            if(numberIdx.size() <= candidates){
+                for(int j= 0;j< k-i;++j){
+                    total+= numberIdx[numberIdx.size()-1 - j].first;
                 }
                 break;
             }
 
 
+            //search from right to left, find first element which second is < can + leftPick
+            //or >= size - candidates - rightPick
 
-
-            for(auto it = costPos.begin();it!=costPos.end();){
-                auto &posSet = it->second;
-
-                //if any < candidates, or >= size - candidates
-                //
-                auto leftPos = posSet.begin();
-                if(*leftPos < candidates + leftPick){
-                    total += (int)(it->first);
-                    posSet.erase(leftPos);
+            for(int idx = numberIdx.size()-1;idx>=0; --idx){
+                bool found = false;
+                if(numberIdx[idx].second < candidates + leftPick){
+                    found = true;
                     ++leftPick;
-                    if(posSet.empty()){
-                        auto erase = it;
-                        ++it;
-                        costPos.erase(erase);
-                        break;
-                    }else{
-                        ++it;
-                        break;
-                    }
-                }
-                //not in left set
-                
-                auto rightPos = posSet.lower_bound(costs.size()- rightPick - candidates);
-                if((rightPos != posSet.end()) || (*rightPos > costs.size()- rightPick - candidates)){
-                    total+= (long long )(it->first);
-                    posSet.erase(rightPos);
+                } else if(numberIdx[idx].second >= costs.size() - rightPick - candidates){
+                    found = true;
                     ++rightPick;
-                    if(posSet.empty()){
-                        auto erase = it;
-                        ++it;
-                        costPos.erase(erase);
-                        break;
-                    }else{
-                        ++it;
-                        break;
-                    }
                 }
-                ++it;
-            } 
 
-
+                if(found){
+                    total+= numberIdx[idx].first;
+                    copy(numberIdx.begin()+idx+1, numberIdx.end(), numberIdx.begin()+idx);
+                    numberIdx.pop_back();
+                    break;
+                }
+            }
         }
 
         return total;
@@ -155,5 +127,18 @@ int main(){
         int k = 3;
         int candidates = 3;
         assert(s.totalCost(costs, k,candidates) == 4);
+    }
+    {
+        vector<int> costs = {19,29,88,2,5,24,60,26,76,24,96,82,97,97,72,35,21,77,82,30,94,55,76,94,51,82,3,89,52,96,72,27,59,57,97,6,46,88,41,52,46,4,17};
+        int k = 1;
+        int candidates = 3;
+        assert(s.totalCost(costs,k,candidates) == 4);
+    }
+    {
+        vector<int> costs= {50,80,34,9,86,20,67,94,65,82,40,79,74,92,84,37,19,16,85,20,79,25,89,55,67,84,3,79,38,16,44,2,54,58};
+        int k = 7;
+        int candidates = 12;
+        assert(s.totalCost(costs,k,candidates) == 95);
+
     }
 }
