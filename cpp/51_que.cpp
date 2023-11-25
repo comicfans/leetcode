0b5cd01b5473e899ece968790e58e505bacba747
comicfans <comcifans44@gmail.com>
2023-11-25 14:49:02 +0200

    51

---
+++ cpp/51_que.cpp
@@ -0,0 +1,93 @@
+#include <vector>
+#include <cassert>
+#include <string>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    vector<vector<string>> solveNQueens(int n) {
+
+        vector<vector<string>> ret;
+
+        vector<string> grid;
+
+        string empty;
+        empty.resize(n,'.');
+        for(int i = 0;i< n;++i){
+            grid.push_back(empty);
+        }
+        
+        recFind(ret, grid,0);
+
+        return ret;
+    }
+
+    void recFind(vector<vector<string>>& ret,vector<string>& existing, int searchIdx){
+
+        if(searchIdx == existing.size()){
+            ret.push_back(existing);
+            return;
+        }
+
+
+        for(int i = 0; i < existing.size();++i){
+            //try to put it at 
+            //y = searchIdx,
+            //x = i;
+            //search conflict at x
+            bool reject = false;
+            for(int check = 0;check < searchIdx;++check){
+                if(existing[check][i]=='Q'){
+                    reject = true;
+                    break;
+                }
+            }
+            if(reject){
+                continue;
+            }
+
+            //search 45 degree conflict 
+
+            for(int check = 0; check < searchIdx;++check){
+
+                int yCheck1 = searchIdx - check - 1;
+                if(yCheck1 <0){
+                    continue;
+                }
+
+
+                int xCheck1 = i - check - 1;
+                if(xCheck1 >=0){
+                    if(existing[yCheck1][xCheck1] == 'Q'){
+                        reject = true;
+                        break;
+                    }
+                }
+
+                int xCheck2 = i + check + 1;
+                if(xCheck2 < existing.size()){
+                    if(existing[yCheck1][xCheck2] == 'Q'){
+                        reject = true;
+                        break;
+                    }
+                }
+            }
+
+            if(reject){
+                continue;
+            }
+
+            existing[searchIdx][i] = 'Q';
+            recFind(ret, existing,searchIdx + 1);
+            existing[searchIdx][i] = '.';
+        }
+    }
+};
+
+int main(){
+    Solution s;
+    auto res = s.solveNQueens(4);
+}
