e6618a74e59b58bd0283d3fb291cc64cc0aade08
comicfans <comcifans44@gmail.com>
2023-11-25 11:22:10 +0200

    42 too slow

--- cpp/42_rain.cpp
+++ cpp/42_rain.cpp
@@ -1,3 +1,4 @@
+#include <numeric>
 #include <vector>
 #include <cassert>
 #include <string>
@@ -10,87 +11,74 @@ class Solution {
 public:
     int trap(vector<int>& height) {
 
-        //throw from left, if is equal or greator
-
-        int leftBegin = 0;
-        for(int throwLeft = 1;throwLeft < height.size();++throwLeft){
-            if(height[throwLeft]>= height[throwLeft - 1]){
-                leftBegin = throwLeft;
-            }else{
-                break;
-            }
+        if(height.size() < 3){
+            return 0;
         }
 
-        auto end = height.erase(height.begin(),height.begin()+leftBegin);
-
-        int rightEnd = height.size();
-        //throw from right, if is equal or greator
-        for(int throwRight = height.size()-2; throwRight >= 0;--throwRight){
-            if(height[throwRight]> height[throwRight+1]){
-                rightEnd = throwRight+1;
-            }else{
-                break;
-            }
-        }
-        height.resize(rightEnd);
+        int ret = 0;
+        multiset<int> thisUHeights;
+        int leftMax = height[0];
+        for(int i = 1;i<height.size();++i){
 
 
-        vector<int> sortHeight = height;
-        sort(sortHeight.begin(),sortHeight.end());
-        sortHeight.resize(unique(sortHeight.begin(),sortHeight.end())-sortHeight.begin());
+            if(height[i]>= height[i-1]){
+                //do realize, or first
 
-        int res = 0;
-        for(int s= 0;s<sortHeight.size();++s){
-            auto splitHeight = sortHeight[s];
-            if(splitHeight == 0){
-                continue;
-            }
-            
-            int heightDelta = (s==0?splitHeight: splitHeight - sortHeight[s-1]);
+                int realizeHeight = min(leftMax, height[i]);
 
-            int leftSide = -1;
-            int possibleWidth = 0;
-
-            int thisLevel = 0;
-
-            for(int i=0;i< height.size();++i){
-
-                if(height[i] < splitHeight){
-                    if(leftSide == -1){
-                        continue;
-                    }else{
-                        ++possibleWidth;
-                        continue;
+                vector<multiset<int>::iterator> toChange;
+                for(auto it = thisUHeights.begin();it!= thisUHeights.end();++it){
+                    auto h = *it;
+                    if(h<= realizeHeight){
+                        toChange.push_back(it);
+                        ret+= realizeHeight - h;
                     }
                 }
 
-                if(leftSide == -1){
-                    leftSide = i;
-                    continue;
-                }else{
-                    //this is right side
-                    thisLevel += possibleWidth * heightDelta;
-                    leftSide = i;
-                    possibleWidth = 0;
+                for(auto p : toChange){
+                    thisUHeights.erase(p);
                 }
+                for(int i=0;i< toChange.size();++i){
+                    thisUHeights.insert(realizeHeight);
+                }
+
+
+
+                if(height[i] >= leftMax){
+                    thisUHeights.clear();
+                    leftMax = height[i];
+                }else{
+                    thisUHeights.insert(height[i]);
+                }
+                continue;
             }
-            res += thisLevel;
+
+            thisUHeights.insert(height[i]);
         }
-        return res;
+
+        return ret;
+
     }
+
 };
 
+#include "parse.h"
 int main(){
 
     Solution s;
     {
-        vector<int> input = {9,6,8,8,5,6,3};
+        auto input = pvi("42_case1.txt",0);
         s.trap(input);
     }
     {
         vector<int> input = {0,7,1,4,6};
         assert(s.trap(input) == 7);
     }
+    {
+        vector<int> input = {9,6,8,8,5,6,3};
+        s.trap(input);
+    }
+    
     {
         vector<int> input = {0,1,0,2,1,0,1,3,2,1,2,1};
         assert(s.trap(input) == 6);
