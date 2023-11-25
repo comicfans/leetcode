fb55342ce3b4257d13f629fece83f25523c03559
comicfans <comcifans44@gmail.com>
2023-11-25 11:29:19 +0200

    42

--- cpp/42_rain.cpp
+++ cpp/42_rain.cpp
@@ -16,7 +16,7 @@ public:
         }
 
         int ret = 0;
-        multiset<int> thisUHeights;
+        map<int,int> thisUHeightCount;
         int leftMax = height[0];
         for(int i = 1;i<height.size();++i){
 
@@ -26,34 +26,34 @@ public:
 
                 int realizeHeight = min(leftMax, height[i]);
 
-                vector<multiset<int>::iterator> toChange;
-                for(auto it = thisUHeights.begin();it!= thisUHeights.end();++it){
-                    auto h = *it;
-                    if(h<= realizeHeight){
+                vector<map<int,int>::iterator> toChange;
+                int changeNumber = 0;
+                for(auto it = thisUHeightCount.begin();it!= thisUHeightCount.end();++it){
+                    auto kv = *it;
+                    if(kv.first < realizeHeight){
                         toChange.push_back(it);
-                        ret+= realizeHeight - h;
+                        ret+= (realizeHeight - kv.first) * kv.second;
+                        changeNumber += kv.second;
                     }
                 }
 
                 for(auto p : toChange){
-                    thisUHeights.erase(p);
+                    thisUHeightCount.erase(p);
                 }
-                for(int i=0;i< toChange.size();++i){
-                    thisUHeights.insert(realizeHeight);
-                }
+                thisUHeightCount[realizeHeight]+= changeNumber;
 
 
 
                 if(height[i] >= leftMax){
-                    thisUHeights.clear();
+                    thisUHeightCount.clear();
                     leftMax = height[i];
                 }else{
-                    thisUHeights.insert(height[i]);
+                    thisUHeightCount[height[i]]++;
                 }
                 continue;
             }
 
-            thisUHeights.insert(height[i]);
+            thisUHeightCount[height[i]]++;
         }
 
         return ret;
@@ -66,6 +66,10 @@ public:
 int main(){
 
     Solution s;
+    {
+        vector<int> input = {4,2,0,3,2,5};
+        assert(s.trap(input) == 9);
+    }
     {
         auto input = pvi("42_case1.txt",0);
         s.trap(input);
@@ -83,6 +87,7 @@ int main(){
         vector<int> input = {0,1,0,2,1,0,1,3,2,1,2,1};
         assert(s.trap(input) == 6);
     }
+
     
     
 }
