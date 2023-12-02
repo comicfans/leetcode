7e66c192bd227e02f6390105b6fcb754a70aabc4
comicfans <comcifans44@gmail.com>
2023-12-02 13:20:44 +0200

    1751 too slow

--- cpp/1751.cpp
+++ cpp/1751.cpp
@@ -10,54 +10,79 @@ class Solution {
 public:
     int maxValue(vector<vector<int>>& events, int k) {
 
+        sort(events.begin(),events.end());
+        map<int, map<int,int>> cache;
 
-        vector<int> chain;
-        return recFind(events,k,chain,0);
+        return recFind(events,k,-1,0,cache);
     }
 
-    int recFind(const vector<vector<int>>& events, int k, vector<int>& chain, int currentSum){
+    int recFind(const vector<vector<int>>& events, int k,int prevEnd, int beginIdx, map<int,map<int,int>>& cache){
         if(k == 0){
-            return currentSum;
+            return 0;
         }
 
         //pick any possible
 
-        int best = currentSum;
-        for(int tryIdx = chain.empty()?0:chain.back()+1; tryIdx < events.size(); ++ tryIdx){
+        int thisBest = 0;
+
+        int tryBegin = 0;
+        if(prevEnd != -1){
+
+            auto searchBegin = upper_bound(events.begin()+ beginIdx,events.end(),vector<int>{prevEnd},[](auto& lhs,auto& rhs ){
+                return lhs[0] < rhs[0];
+            });
+            tryBegin = searchBegin - events.begin();
+        }
+
+        for(int tryIdx = tryBegin; tryIdx < events.size(); ++ tryIdx){
 
             auto& ev = events[tryIdx];
 
-            bool hasOverlap = false;
-            for(int checkOverlap = 0;checkOverlap < chain.size();++checkOverlap){
 
-                auto& picked = events[chain[checkOverlap]];
-
-                if(max(ev[0],picked[0]) <= min(ev[1],picked[1])){
-                    hasOverlap = true;
-                    break;
+            int thisRes = 0;
+            bool hasCache = false;
+            auto cachePos = cache.find(tryIdx);
+            if(cachePos != cache.end()){
+                auto kPos = cachePos->second.find(k);
+                if(kPos != cachePos->second.end()){
+                    hasCache = true;
+                    thisRes = kPos->second;
                 }
             }
 
-            if(hasOverlap){
-                continue;
+            if(!hasCache){
+                int subRes = recFind(events, k-1, ev[1] ,tryIdx + 1,cache);
+                thisRes = ev[2] + subRes;
+                cache[tryIdx][k] = thisRes;
             }
 
-            chain.push_back(tryIdx);
-            currentSum += ev[2];
-
-            int thisRes = recFind(events, k-1, chain, currentSum);
-
-            best = max(thisRes, best);
-            currentSum -= ev[2];
-            chain.pop_back();
+            
+            thisBest = max(thisRes, thisBest);
         }
-
-        return best;
+        return thisBest;
     }
 };
 
+#include "parse.h"
 int main(){
     Solution s;
+    {
+        auto input = pvvi("1751_case1.txt", 1);
+        int k = 50;
+        s.maxValue(input,k);
+    }
+    {
+        auto input = pvvi("1751_case1.txt", 0);
+        int k = 50;
+        s.maxValue(input,k);
+    }
+    {
+        vector<vector<int>> input= {{1,1,1},{2,2,2},{3,3,3},{4,4,4}};
+        int k = 3;
+        int expect =  9;
+        assert(s.maxValue(input,k) == expect);
+
+    }
     {
 
         vector<vector<int>> input= {{1,2,4},{3,4,3},{2,3,1}};
