48de79b82df21f82d51850fbc48a6900d89fa427
comicfans <comcifans44@gmail.com>
2023-12-11 17:48:17 +0200

    1287

---
+++ cpp/1200_1299/1287.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <cassert>
+#include <string>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+public:
+    int findSpecialInteger(vector<int>& arr) {
+
+
+        int minNumber = (arr.size() + 4)/ 4;
+
+        map<int,int> hits;
+        for(int sample = 0;sample < arr.size(); sample+= minNumber){
+
+            int thisValue = arr[sample];
+
+            auto range = equal_range(arr.begin(),arr.end(),thisValue);
+
+            if(range.second - range.first >= minNumber){
+                return thisValue;
+            }
+
+        }
+        assert(false);
+        
+    }
+};
