5e15ad59c367e9e0221b862a59193adaa172ea3d
comicfans <comcifans44@gmail.com>
2024-01-24 09:21:40 +0200

    100

---
+++ cpp/0_99/100.cpp
@@ -0,0 +1,37 @@
+#include <vector>
+#include <cassert>
+#include <string>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        
+        if(bool(p)!=bool(q)){
+            return false;
+        }
+
+        if(!p){
+            return true;
+        }
+
+        if(p->val != q->val){
+            return false;
+        }
+
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+
+    }
+};
