7094b7e943a863b4a52df0c948fdb32d6ec2dd62
comicfans <comcifans44@gmail.com>
2024-03-05 10:22:51 +0200

    350

---
+++ cpp/300_399/350.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <cassert>
+#include <string>
+#include <set>
+#include <map>
+#include <algorithm>
+
+using namespace std;
+class Solution {
+
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        //nums1.resize(unique(nums1.begin(),nums1.end()) - nums1.begin());
+
+        sort(nums2.begin(),nums2.end());
+        //nums2.resize(unique(nums2.begin(),nums2.end()) - nums2.begin());
+
+        vector<int> ret;
+        set_intersection(nums1.begin(),nums1.end(),nums2.begin(),nums2.end(), back_inserter(ret));
+        return ret;
+    }
+};
+   
