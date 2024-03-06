de3f33b046301fa2b32d7d01e5efb8ae59a876a3
comicfans <comcifans44@gmail.com>
2024-03-06 16:31:13 +0200

    354 faster still too slow

--- cpp/300_399/354.cpp
+++ cpp/300_399/354.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <iterator>
 #include <vector>
 #include <cassert>
 #include <string>
@@ -5,91 +7,107 @@
 #include <map>
 #include <algorithm>
 
+
+
 using namespace std;
+
 class Solution {
 public:
     int maxEnvelopes(vector<vector<int>>& envelopes) {
 
         sort(envelopes.begin(),envelopes.end());
 
-        vector<int> best(envelopes.size());
+        vector<pair<int,int>> prePost(envelopes.size());
 
+        int globalBest = 0;
+        for(int i = 0;i < envelopes.size(); ++i){
+            if(prePost[i].first!=0){
+                continue;
+            }
+            rec(envelopes, prePost, i , 0, 0,0,globalBest);
+            globalBest = max(globalBest, prePost[i].first + prePost[i].second);
+        }
 
-        return  rec(envelopes, 0, best,0,0, 0);
+            
+        int ret = 0;
+        for(const auto& v:prePost){
+            ret = max(ret, v.second + v.first);
+        }
+
+        return ret;
+
         
     }
 
-    int rec(const vector<vector<int>>& envelopes, int beginSearch,vector<int>& best, int parentW,int parentH, int currentLength){
 
-        if(beginSearch == envelopes.size()){
-            return 0;
-        }
 
-        int useablePos = beginSearch;
+    int rec(const vector<vector<int>>& envelopes, 
+             vector<pair<int,int>>& prePost,
+             int beginSearch,
+             int parentW,int parentH, int currentLength, int& globalBest){
+
 
             //find next useable
 
+        int useablePos = beginSearch;
+        int subMax = 0;
+        int prevW = parentW;
             while(useablePos < envelopes.size()){
-                vector<int> nextV = {parentW+1,parentH+1};
-                auto lower = lower_bound(envelopes.begin() + useablePos, envelopes.end(), nextV);
+                vector<int> nextV = {prevW+1,parentH+1};
+                auto wBigger = lower_bound(envelopes.begin() + useablePos, envelopes.end(), nextV) - envelopes.begin();
 
-                if(lower  == envelopes.end()){
-                    useablePos= envelopes.size();
-                    break;
-                }
-                assert((*lower)[0]> parentW);
-
-                if((*lower)[1] > parentH){
-                    useablePos= lower - envelopes.begin();
-
-                    assert(envelopes[useablePos][0]>parentW);
-                    assert(envelopes[useablePos][1]>parentH);
+                if(wBigger == envelopes.size()){
                     break;
                 }
 
-                auto sameWidthEnd = lower_bound(lower+1, envelopes.end(), vector<int>{(*lower)[0]+1,0});
+                //assert(envelopes[wBigger][0]> prevW);
 
 
-                auto bigger = lower_bound(lower, sameWidthEnd, vector<int>{(*lower)[0], parentH+1});
+                auto hBigger = lower_bound(envelopes.begin()+wBigger, envelopes.end(),
+                                          vector<int>{envelopes[wBigger][0], parentH+1}) - envelopes.begin();
 
-                if(bigger == sameWidthEnd){
-                    useablePos= sameWidthEnd - envelopes.begin();
-                    continue;
+                if(hBigger == envelopes.size()){
+                    break;
                 }
 
-                useablePos=bigger - envelopes.begin();
+                bool stopTry= false;
+                for(useablePos = hBigger;useablePos < envelopes.size(); ++useablePos){
 
-                assert(envelopes[useablePos][0]>parentW);
-                assert(envelopes[useablePos][1]>parentH);
+                    if(envelopes.size() - useablePos + currentLength <= globalBest){
+                    //stopTry = true;
+                    break;
+                }
+
+                        //assert(envelopes[useablePos][0]> prevW);
+                        if(envelopes[useablePos][1]<=parentH){
+                            prevW =envelopes[hBigger][0]-1;
+                            ++useablePos;
+                            break;
+                    }
+
+                    if(prePost[useablePos].second > 0){
+                        
+                        prePost[useablePos].first = max(prePost[useablePos].first, currentLength);
+                        subMax = max(subMax, prePost[useablePos].second);
+                        globalBest = max(globalBest, subMax + currentLength);
+                        continue;
+                    }
+
+                    //assert(prePost[useablePos].first == 0);
+                    prePost[useablePos].first = currentLength;
+                    prePost[useablePos].second = rec(envelopes, prePost, useablePos+1, envelopes[useablePos][0], envelopes[useablePos][1], currentLength+1,globalBest)+1;
+                    subMax = max(subMax, prePost[useablePos].second);
+                }
+                
+            if(stopTry){
                 break;
             }
-
-
-
-        if(useablePos == envelopes.size()){
-            return 0;
-        }
-        
-        
-
-        int useThisRes = best[useablePos];
-        if(useThisRes==0){
-
-            auto subRes = rec(envelopes, useablePos+1, best, envelopes[useablePos][0],envelopes[useablePos][1], currentLength+1);
-            best[useablePos] = subRes+ 1;
-            useThisRes = best[useablePos];
-        }
-
-        //find next  useable
-        
-        //because try same width bigger height is useless, so we should try
-        //next width
-
-        auto notUseThisRes = rec(envelopes,useablePos+1, best,envelopes[useablePos][0],parentH,  currentLength);
-
-        return max(best[useablePos],notUseThisRes);
-
+            }
+        return subMax;
     }
+
+
+
 };
 
 #include "../parse.h"
@@ -98,10 +116,11 @@ int main(){
     Solution s;
     {
 
-        auto env = pvvi("354_case.txt");
-        int res = s.maxEnvelopes(env);
-
+        vector<vector<int>> env = {{1,1},{1,1},{1,1}};
+        assert(s.maxEnvelopes(env) == 1);
     }
+    
+    
     {
         vector<vector<int>> env = {{1,15},{7,18},{7,6},{7,100},{2,200},{17,30},{17,45},{3,5},{7,8},{3,6},{3,10},{7,20},{17,3},{17,45}};
 
@@ -109,13 +128,19 @@ int main(){
         assert(res == 3);
     }
     {
-vector<vector<int>> env = {{15,22},{8,34},{44,40},{9,17},{43,23},{4,7},{20,8},{30,46},{39,36},{45,14},{24,19},{24,36},{31,34},{32,19},{29,13},{16,48},{8,36},{44,2},{11,5},{2,50},{29,6},{18,38},{15,49},{22,37},{6,20},{25,11},{1,50},{19,40},{45,35},{37,21},{4,29},{40,5},{4,49},{1,45},{14,32},{14,37},{23,22},{31,21},{2,36},{43,4},{21,32},{41,2},{44,32},{36,20},{22,45},{3,41},{44,29},{29,33},{42,2},{38,17},{43,26},{30,15},{28,12},{33,30},{49,7},{8,14},{1,9},{41,25},{7,15},{26,32},{11,33},{12,45},{33,7},{16,34},{39,1},{20,49},{50,45},{14,29},{50,41},{1,45},{14,43},{49,20},{41,37},{43,22},{45,19},{20,21},{28,19},{2,1},{7,49},{3,3},{49,48},{34,35},{10,2}};
+    vector<vector<int>> env = {{15,22},{8,34},{44,40},{9,17},{43,23},{4,7},{20,8},{30,46},{39,36},{45,14},{24,19},{24,36},{31,34},{32,19},{29,13},{16,48},{8,36},{44,2},{11,5},{2,50},{29,6},{18,38},{15,49},{22,37},{6,20},{25,11},{1,50},{19,40},{45,35},{37,21},{4,29},{40,5},{4,49},{1,45},{14,32},{14,37},{23,22},{31,21},{2,36},{43,4},{21,32},{41,2},{44,32},{36,20},{22,45},{3,41},{44,29},{29,33},{42,2},{38,17},{43,26},{30,15},{28,12},{33,30},{49,7},{8,14},{1,9},{41,25},{7,15},{26,32},{11,33},{12,45},{33,7},{16,34},{39,1},{20,49},{50,45},{14,29},{50,41},{1,45},{14,43},{49,20},{41,37},{43,22},{45,19},{20,21},{28,19},{2,1},{7,49},{3,3},{49,48},{34,35},{10,2}};
+
+        //(2,1)(3,3)(4,7)(20,8)(25,11)(28,12)(29,13)(30,15)(32,19)(36,20)(37,21)(41,25)(43,26)(44,29)(45,35)(49,48)
+
+        //int res2 = s.maxEnvelopes2(env) ;
         int res = s.maxEnvelopes(env) ;
         assert(res == 16);
     }
     {
         vector<vector<int>> env = {{7,10},{14,7},{3,16},{6,3},{7,9},{5,2},{10,1},{6,11},{18,20}};
         int res = s.maxEnvelopes(env) ;
+        //int res2= s.maxEnvelopes2(env);
+        assert(res == 4);
     }
     {
         vector<vector<int>> env = {{5,4},{6,4},{6,7},{2,3}};
@@ -124,7 +149,16 @@ vector<vector<int>> env = {{15,22},{8,34},{44,40},{9,17},{43,23},{4,7},{20,8},{3
     }
     {
 
-        vector<vector<int>> env = {{1,1},{1,1},{1,1}};
-        assert(s.maxEnvelopes(env) == 1);
+        auto env = pvvi("354_case.txt",0);
+        int res = s.maxEnvelopes(env);
+        cout<<"0:"<<res<<endl;
+
+    }
+    {
+
+        auto env = pvvi("354_case.txt",1);
+        int res = s.maxEnvelopes(env);
+        cout<<"1:"<<res<<endl;
+
     }
 }
