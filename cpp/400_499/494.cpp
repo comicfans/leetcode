1c417420bf0fd0360ae2f9b0d91dd5c37e5f8f7f
comicfans44 <comicfans44@gmail.com>
2024-03-25 17:28:53 +0200

    494 faster a little ?

--- cpp/400_499/494.cpp
+++ cpp/400_499/494.cpp
@@ -11,20 +11,13 @@ public:
     int findTargetSumWays(vector<int>& nums, int target) {
 
 
-        map<int,int> numberCount;
-        for(auto v: nums){
-            numberCount[v]++;
-        }
-
-
-
-        return rec(numberCount, numberCount.begin(), target);
+        return rec(nums, 0, target);
         
     }
 
-    int rec(const map<int,int>& numCount, const map<int,int>::const_iterator pos, int target){
+    int rec(const vector<int>& nums, int pos, int target){
 
-        if(pos == numCount.end()){
+        if(pos == nums.size()){
 
             if(target == 0){
                 return 1;
@@ -33,68 +26,13 @@ public:
             return 0;
         }
 
-        int value = pos->first;
-        int count = pos->second;
+        int addRes = rec(nums, pos+1, target - nums[pos]);
 
-        int ret = 0;
+        int subRes = rec(nums, pos+1, target + nums[pos]);
 
-        int zeroSub = 0;
-        for(int i = 0; i<= count; ++i){
-
-            int thisValue = (i  - (count - i)) * value;
-
-            int left = target - thisValue;
-            
-            auto temp = pos;
-            ++temp;
-
-            int sub ;
-            if(value == 0 && i!= 0){
-                sub = zeroSub;
-            }else{
-                sub = rec(numCount, temp, left);
-                zeroSub = sub;
-            }
-
-            if(value == 0 && zeroSub == 0){
-                return 0;
-            }
+        return addRes + subRes;
 
 
-            if(sub == 0){
-                continue;
-            }
-
-            //pick i=4 out of count=5
-            //
-            // 5*4*3*2 / 4*3*2*1 = 
-            //
-            //
-            // choose i out of count
-            // !count /  !i  / !(count - i)
-            int res = 1;
-            int big = max(i, count - i);
-            int small = min(i, count - i);
-            for(int j = count; j > big; --j){
-                res *= j;
-                if(small > 1 && res % small == 0){
-                    res /= small;
-                    --small;
-                }
-            }
-
-            while(small > 1){
-                assert(res % small == 0);
-                res /= small;
-                --small;
-            }
-
-            ret += res * sub;
-
-            
-        }
-
-        return ret;
     }
 };
 
