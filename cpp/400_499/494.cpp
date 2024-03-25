a5fa62be1e15123be9ba24c15f5c5a172e818606
comicfans44 <comicfans44@gmail.com>
2024-03-25 17:38:21 +0200

    494 faster a little

--- cpp/400_499/494.cpp
+++ cpp/400_499/494.cpp
@@ -10,14 +10,28 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
 
+        sort(nums.begin(),nums.end());
 
-        return rec(nums, 0, target);
+        vector<pair<int,int>> numberCount;
+
+        for(int pos = 0; pos < nums.size(); ){
+
+            auto end = upper_bound(nums.begin() + pos + 1, nums.end(), nums[pos]) - nums.begin();
+
+            numberCount.push_back({nums[pos], end - pos});
+            pos = end;
+        }
+        
+
+
+
+        return rec(numberCount, 0, target);
         
     }
 
-    int rec(const vector<int>& nums, int pos, int target){
+    int rec(const vector<pair<int,int>>& numCount, int pos, int target){
 
-        if(pos == nums.size()){
+        if(pos == numCount.size()){
 
             if(target == 0){
                 return 1;
@@ -26,13 +40,73 @@ public:
             return 0;
         }
 
-        int addRes = rec(nums, pos+1, target - nums[pos]);
+        int value = numCount[pos].first;
+        int count = numCount[pos].second;
 
-        int subRes = rec(nums, pos+1, target + nums[pos]);
+        int ret = 0;
 
-        return addRes + subRes;
+        int zeroSub = 0;
+        for(int i = 0; i<= count; ++i){
+
+            int thisValue = (i  - (count - i)) * value;
+
+            int left = target - thisValue;
+            
+            auto temp = pos;
+            ++temp;
+
+            int sub ;
+            if(value == 0 && i!= 0){
+                sub = zeroSub;
+            }else{
+                sub = rec(numCount, temp, left);
+                zeroSub = sub;
+            }
+
+            if(value == 0 && zeroSub == 0){
+                return 0;
+            }
 
 
+            if(sub == 0){
+                continue;
+            }
+
+            if(value == 0){
+                ret = pow(2, count) * sub;
+                break;
+            }
+
+            //pick i=4 out of count=5
+            //
+            // 5*4*3*2 / 4*3*2*1 = 
+            //
+            //
+            // choose i out of count
+            // !count /  !i  / !(count - i)
+            int res = 1;
+            int big = max(i, count - i);
+            int small = min(i, count - i);
+            for(int j = count; j > big; --j){
+                res *= j;
+                if(small > 1 && res % small == 0){
+                    res /= small;
+                    --small;
+                }
+            }
+
+            while(small > 1){
+                assert(res % small == 0);
+                res /= small;
+                --small;
+            }
+
+            ret += res * sub;
+
+            
+        }
+
+        return ret;
     }
 };
 
