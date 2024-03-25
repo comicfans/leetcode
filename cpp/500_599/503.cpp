0e34157382e7dc5e81558e634652e9acc7bb067f
comicfans44 <comicfans44@gmail.com>
2024-03-25 23:56:56 +0200

    503 faster

--- cpp/500_599/503.cpp
+++ cpp/500_599/503.cpp
@@ -10,52 +10,74 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
 
-        vector<int> ret(nums.size());
+        vector<int> ret(nums.size(),INT_MAX);
 
-        int currentMax = numeric_limits<int>::min();
         
-        map<int,set<int>> valueIdx;
-
-        for(int idx = 0; idx < nums.size(); ++idx){
+        vector<int> peak;
+        peak.reserve(nums.size());
+        peak.push_back(0);
+        for(int idx = 1; idx < nums.size(); ++idx){
             int v = nums[idx];
 
-            auto p = valueIdx.lower_bound(v);
 
-            for(auto it = valueIdx.begin(); it!= p; ){
-                for(auto r: it->second){
-                    ret[r] = v;
+            if(v > nums[idx-1]){
+                //fill back same value
+                for(; !peak.empty() && nums[peak.back()] < v;){
+                    int last = peak.back();
+                    peak.pop_back();
+                    if(ret[last]==INT_MAX){
+                        ret[last] = v;
+                    }
                 }
-                auto temp = it;
-                ++it;
-                valueIdx.erase(temp);
+
+                peak.push_back(idx);
+
+                //if prev is less than nums[idx-1], thus no need to fill
+                //if prev is > v, no need to fill?
+                continue;
             }
 
-            valueIdx[v].insert(idx);
+            peak.push_back(idx);
         }
 
-        if(!valueIdx.empty()){
-
-            for(int c = 0; c < nums.size(); ++c){
-
-                int v = nums[c];
-
-                auto p = valueIdx.lower_bound(v);
-                for(auto it = valueIdx.begin(); it!= p; ){
-                    for(auto r: it->second){
-                        ret[r] = v;
-                    }
-                    auto temp = it;
-                    ++it;
-                    valueIdx.erase(temp);
-                }
+        if(peak.size() == nums.size()){
+            ret[0] = -1;
+            for(int i = 1; i< ret.size(); ++i){
+                ret[i] = (nums.front() > nums[i]? nums.front(): -1);
             }
 
-            for(const auto& [key,value]: valueIdx){
-                for(auto i: value){
-                    ret[i] = -1;
+            return ret;
+        }
+
+        ret[peak.front()] = -1;
+        int lessIdx = 1;
+        for(; lessIdx < peak.size(); ++lessIdx){
+            int thisI = peak[lessIdx];
+            if(nums[thisI] == nums[peak.front()]){
+                ret[thisI] = -1;
+                continue;
+            }
+            break;
+        }
+
+        int peakIdx = peak.size() - 1;
+
+        //now look for missed
+        for(int j = 0; j < nums.size() && peakIdx >= lessIdx; ++j){
+            
+            
+            for(int i = peak[peakIdx]; nums[j] > nums[i]; ){
+                ret[i] = nums[j];
+                if(peakIdx == lessIdx){
+                    break;
                 }
+                --peakIdx;
+                i = peak[peakIdx];
             }
 
+            if(peakIdx == lessIdx && ret[peak[peakIdx]] != INT_MAX){
+                break;
+            }
         }
 
         return ret;
@@ -64,6 +86,26 @@ public:
 
 int main(){
     Solution s;
+    {
+        vector<int> input = {100,1,11,1,120,111,123,1,-1,-100};
+        vector<int> expect = {120,11,120,120,123,123,-1,100,100,100};
+        auto res = s.nextGreaterElements(input);
+        assert(expect == res);
+    }
+    {
+
+        vector<int> input = {1,1,1,1,1};
+        vector<int> expect = {-1,-1,-1,-1,-1};
+        auto res = s.nextGreaterElements(input);
+        assert(expect == res);
+    }
+    {
+
+        vector<int> input = {1,2,3,2,1};
+        vector<int> expect = {2,3,-1,3,2};
+        auto res = s.nextGreaterElements(input);
+        assert(expect == res);
+    }
     {
         vector<int> input = {1,2,3,4,3};
         vector<int> expect = {2,3,4,-1,4};
