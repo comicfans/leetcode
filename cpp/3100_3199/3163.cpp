3e0db3a1ff352ddb39f7d0a10153ed40231e9a02
comicfans44 <comicfans44@gmail.com>
2024-06-02 15:41:22 +0200

    3163

--- cpp/3100_3199/3163.cpp
+++ cpp/3100_3199/3163.cpp
@@ -1,78 +1,67 @@
-#include <vector>
-#include <cassert>
-#include <string>
-#include <set>
-#include <map>
 #include <algorithm>
+#include <cassert>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
 
 using namespace std;
 
-
 #ifdef LEETCODE
 #include "../parse.h"
-#endif 
+#endif
 
-
-
-
-//jj
+// jj
 class Solution {
 public:
-    string compressedString(string word) {
+  string compressedString(string word) {
 
-        string ret;
-        
-        int count = 0;
-        char prev = ' ';
-        for(auto  c:word){
-            if(c != prev || count == 9){
+    string ret;
 
-            while(count > 0){
-                int num = min(9, count);
-                ret.push_back(num +'0');
-                ret.push_back(prev);
-                count -= num;
-            }
+    int count = 0;
+    char prev = ' ';
+    for (auto c : word) {
+      if (c != prev || count == 9) {
 
-            }
-
-            if(prev != c){
-                count = 1;
-            }else{
-                ++count;
-            }
-
-            prev = c;
+        while (count > 0) {
+          int num = min(9, count);
+          ret.push_back(num + '0');
+          ret.push_back(prev);
+          count -= num;
         }
-while(count > 0){
-                int num = min(9, count);
-                ret.push_back(num +'0');
-                ret.push_back(prev);
-                count -= num;
-            }
-        return ret;
+      }
+
+      if (prev != c) {
+        count = 1;
+      } else {
+        ++count;
+      }
+
+      prev = c;
     }
+    while (count > 0) {
+      int num = min(9, count);
+      ret.push_back(num + '0');
+      ret.push_back(prev);
+      count -= num;
+    }
+    return ret;
+  }
 };
 
-
-
-
-
-
-
 #ifdef LEETCODE
-int main(){
-    Solution s;
-    {
-        string input ="aaaaaaaaaaaaaabb";
-        assert(s.compressedString(input)=="9a5a2b");
-        //VI = {};
-        //VVI = {};
-    }
-    {
-        //VI = {};
-        //VVI = {};
-    }
+int main() {
+  Solution s;
+  {
+    string input = "aaaaaaaaaaaaaabb";
+    assert(s.compressedString(input) == "9a5a2b");
+    // VI = {};
+    // VVI = {};
+  }
+  {
+    // VI = {};
+    // VVI = {};
+  }
 }
-#endif 
-
+#endif
