f9dcac3446e520d3d86bb8691a9eade0715cfa96
comicfans44 <comicfans44@gmail.com>
2024-06-02 15:39:53 +0200

    2894

--- cpp/2800_2899/2894.cpp
+++ cpp/2800_2899/2894.cpp
@@ -1,21 +1,32 @@
-#include <vector>
 #include <cassert>
-#include <string>
-#include <set>
-#include <map>
-#include <algorithm>
 
-using namespace std;
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
 
-        int all = (1+n) * n / 2;
+        int maxDiv = n / m; 
+        int totalSum = (1+n)* n /2;
 
-        int group = n / m;
-        int div = (1+group) * group / 2 * m;
+        int sumOfDiv= (1+maxDiv) * maxDiv / 2 * m;
 
-        return all - div - div;
+        int another = totalSum - sumOfDiv;
 
+        return sumOfDiv - another;
+        
     }
 };
+
+int main(){
+
+    int input[][3] = {{10,3,19},{ 5,6,15}, {5,1,-15}};
+
+    Solution s;
+
+    for(auto& tup: input){
+
+        int res = s.differenceOfSums(tup[0], tup[1]);
+        assert(res == tup[2]);
+
+    }
+
+}
