3615dea07644a26d197443187e794f850878b2a0
comicfans44 <comicfans44@gmail.com>
2024-06-15 12:53:52 +0200

    3040

--- cpp/3100_3199/3040.cpp
+++ cpp/3100_3199/3040.cpp
@@ -26,45 +26,45 @@ public:
       return 0;
     }
 
+    const auto head_sum = nums[begin] + nums[begin + 1];
+    const auto head_tail_sum = nums[begin] + nums[end - 1];
+    const auto tail_sum = nums[end - 1] + nums[end - 2];
+
     if (sum != 0) {
+
       auto pos = cache.find({sum, begin, end});
       if (pos != cache.end()) {
         return pos->second;
       }
-    }
-    int used_sum = sum;
 
-    int best = 0;
-    int head_res = 0;
-    int head_sum = nums[begin] + nums[begin + 1];
-    if (head_sum == sum || sum == 0) {
-      head_res = 1 + rec(nums, begin + 2, end, head_sum, cache);
-      if (head_res > best) {
-        best = head_res;
-        used_sum = head_sum;
+      int best = 0;
+      if (head_sum == sum) {
+        best = 1 + rec(nums, begin + 2, end, sum, cache);
       }
-    }
-    int head_tail_res = 0;
-    int head_tail_sum = nums[begin] + nums[end - 1];
-    if (head_tail_sum == sum || sum == 0) {
-      head_tail_res = 1 + rec(nums, begin + 1, end - 1, head_tail_sum, cache);
-      if (head_tail_res > best) {
-        best = head_tail_res;
-        used_sum = head_tail_sum;
+
+      if (head_tail_sum == sum) {
+        int head_tail_res = 1 + rec(nums, begin + 1, end - 1, sum, cache);
+        best = max(best, head_tail_res);
       }
+
+      if (tail_sum == sum && tail_sum != head_sum) {
+        int tail_res = 1 + rec(nums, begin, end - 2, sum, cache);
+        best = max(best, tail_res);
+      }
+
+      cache[{sum, begin, end}] = best;
+      return best;
     }
 
-    int tail_res = 0;
-    int tail_sum = nums[end - 1] + nums[end - 2];
-    if (tail_sum == sum || sum == 0) {
-      tail_res = 1 + rec(nums, begin, end - 2, tail_sum, cache);
-      if (tail_res > best) {
-        best = tail_res;
-        used_sum = tail_sum;
-      }
-    }
+    int best = 1 + rec(nums, begin + 2, end, head_sum, cache);
 
-    cache[{used_sum, begin, end}] = best;
+    int head_tail_res = 1 + rec(nums, begin + 1, end - 1, head_tail_sum, cache);
+    best = max(best, head_tail_res);
+
+    if (tail_sum != head_sum) {
+      int tail_res = 1 + rec(nums, begin, end - 2, tail_sum, cache);
+      best = max(best, tail_res);
+    }
     return best;
   }
 };
@@ -73,11 +73,21 @@ public:
 int main() {
   Solution s;
   {
+
+    VI input = {3, 3, 7, 3, 2, 4, 2, 3};
+    auto res = s.maxOperations(input);
+    assert(res == 2);
+  }
+  {
     VI input = {2, 2, 1, 2, 1};
     auto res = s.maxOperations(input);
     assert(res == 2);
   }
   {
+
+    VI input = {1, 1, 2, 3, 2, 2, 1, 3, 3};
+    auto res = s.maxOperations(input);
+    assert(res == 4);
     // VI = {};
     // VVI = {};
   }
