3bc597cee6cd54b01f288e0a9d0a203203389654
comicfans44 <comicfans44@gmail.com>
2024-06-22 14:19:42 +0200

    3065

---
+++ cpp/3000_3099/3065.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <cassert>
+#include <map>
+#include <set>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#ifdef LEETCODE
+#include "../parse.h"
+#endif
+
+// jj
+class Solution {
+public:
+  int minOperations(vector<int> &nums, int k) {
+    return count_if(nums.begin(), nums.end(),
+                    [k](const auto v) { return v < k; });
+  }
+};
+
+#ifdef LEETCODE
+int main() {
+  Solution s;
+  {
+    // VI = {};
+    // VVI = {};
+  }
+  {
+    // VI = {};
+    // VVI = {};
+  }
+}
+#endif
