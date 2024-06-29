344466be8215f4f38fa8cfbc6e88aea1016e6790
comicfans44 <comicfans44@gmail.com>
2024-06-29 19:54:51 +0200

    611

--- cpp/600_699/611.cpp
+++ cpp/600_699/611.cpp
@@ -15,23 +15,26 @@ using namespace std;
 class Solution {
 public:
   int triangleNumber(vector<int> &nums) {
+    if (nums.size() < 3) {
+      return 0;
+    }
     sort(nums.begin(), nums.end());
     int ret = 0;
-    for (int i = 0; i < nums.size(); ++i) {
+    for (int i = 0; i < nums.size() - 2; ++i) {
       int side0 = nums[i];
-      for (int j = i + 1; j < nums.size(); ++j) {
+      for (int j = i + 1; j < nums.size() - 1; ++j) {
         int side1 = nums[j];
 
         int side2_max_possible = side0 + side1 - 1;
         const auto pos =
-            lower_bound(nums.begin() + j + 1, nums.end(), side2_max_possible) -
+            upper_bound(nums.begin() + j + 1, nums.end(), side2_max_possible) -
             nums.begin();
 
         int this_count = pos - j - 1;
         ret += this_count;
       }
     }
-        return ret;
+    return ret;
   }
 };
 
@@ -39,10 +42,12 @@ public:
 int main() {
   Solution s;
   {
-    // VI = {};
-    // VVI = {};
+    VI nums = {2, 2, 3, 4};
+    assert(s.triangleNumber(nums) == 3);
   }
   {
+    VI nums = {4, 2, 3, 4};
+    assert(s.triangleNumber(nums) == 4);
     // VI = {};
     // VVI = {};
   }
